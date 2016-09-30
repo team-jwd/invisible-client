8675b69616fc6f8f4f04a70e33b5d88269f955d0
Daniel King <daniel.oliver.king@gmail.com>
2016-09-30 11:06:36 -0700

    Prototyped a basic app that communicates with a server and includes user interaction

--- electron-prototyping/main.js
+++ electron-prototyping/main.js
@@ -7,9 +7,9 @@ let mainWindow = null
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL('http://localhost:3000/')
 
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
     mainWindow = null
